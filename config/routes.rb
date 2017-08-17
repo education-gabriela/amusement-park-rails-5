7cbb4f4b0bfe950e8d22dabac703e4541b694cf5
gabidavila <gabidavila@gmail.com>
2017-08-17 19:12:47 -0400

    added controllers and login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "/", controller: "static", action: "index"
+
+  get "/signin", controller: "sessions", action: "new"
+  get "/signup", controller: "users", action: "new"
 end
