759af4ef20758a5448ab30a346a75bef4a05066e
gabidavila <gabidavila@gmail.com>
2017-08-17 19:28:03 -0400

    validating login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?, :is_admin?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def is_authenticated?
+    if !logged_in?
+      flash[:message] = 'You must be logged in to view this content'
+      redirect_to root_path
+    end
+  end
+
+  def is_admin?
+    current_user && current_user.admin
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,15 @@ class SessionsController < ApplicationController
   def new
 
   end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:message] = "Invalid combination"
+      redirect_to signin_path
+    end
+  end
 end
