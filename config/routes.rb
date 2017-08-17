5088d14a4cc87431c433f0cce9bfd404295705d1
gabidavila <gabidavila@gmail.com>
2017-08-17 19:38:28 -0400

    added sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show]
+  resources :users, only: [:show, :new, :create]
 
   root "/", controller: "static", action: "index"
 
   get "/signin", controller: "sessions", action: "new"
   post "/signin", controller: "sessions", action: "create"
-
-  get "/signup", controller: "users", action: "new"
 end
