6021e76c8420095c91d9251cd7f388cb2fe0c4f0
gabidavila <gabidavila@gmail.com>
2017-08-18 16:20:34 -0400

    crud for attractions

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,5 +1,6 @@
 class AttractionsController < ApplicationController
   before_action :is_authenticated?, except: [:index]
+
   def index
     @attractions = Attraction.order(:name)
   end
@@ -21,5 +22,39 @@ class AttractionsController < ApplicationController
     end
 
     redirect_to user_path(current_user)
+  end
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+
+    if @attraction.save
+      redirect_to attraction_path(@attraction)
+    else
+      redirect_to new_attraction_path
+    end
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+
+    if @attraction.update(attraction_params)
+      redirect_to attraction_path(@attraction)
+    else
+      redirect_to edit_attraction_path
+    end
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :nausea_rating, :happiness_rating, :min_height, :tickets)
   end
 end
